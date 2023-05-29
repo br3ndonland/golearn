b5e947b96bce7a9304afe718256d3d4c45f5897f
Brendon Smith <bws@bws.bio>
2023-05-29 16:03:18 -0400

    Finish hello world example
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/hello-world

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -2,10 +2,31 @@ package main
 
 import "fmt"
 
-func Hello() string {
-	return "Hello, world"
+const englishHelloPrefix = "Hello, "
+const french = "French"
+const frenchHelloPrefix = "Bonjour, "
+const spanish = "Spanish"
+const spanishHelloPrefix = "Hola, "
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World!"
+	}
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("Brendon", ""))
 }
--- helloworld/hello_test.go
+++ helloworld/hello_test.go
@@ -2,10 +2,32 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+}
+
+func TestHello(t *testing.T) {
+	t.Run("say hello with a name argument", func(t *testing.T) {
+		got := Hello("Name", "")
+		want := "Hello, Name"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World!' when no name is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Nathalie", "French")
+		want := "Bonjour, Nathalie"
+		assertCorrectMessage(t, got, want)
+	})
 }
