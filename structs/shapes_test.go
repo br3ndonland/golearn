626ab87ce078a61351c27e7516464b4f4a02b1e3
Brendon Smith <bws@bws.bio>
2023-05-30 18:55:24 -0400

    Start structs example

---
+++ structs/shapes_test.go
@@ -0,0 +1,16 @@
+package main
+
+import "testing"
+
+func assertCorrectFloat(t testing.TB, got, want float64) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestPerimeter(t *testing.T) {
+	got := Perimeter(10.0, 10.0)
+	want := 40.0
+	assertCorrectFloat(t, got, want)
+}
