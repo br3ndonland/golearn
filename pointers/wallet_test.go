d6f9cdc2a9929676426169767ee49ece24811552
Brendon Smith <bws@bws.bio>
2023-07-15 15:03:38 -0400

    Add pointers example and test

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -1,1 +1,18 @@
 package pointers
+
+import "testing"
+
+func assertCorrectBalance(t testing.TB, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+	wallet.Deposit(10)
+	got := wallet.Balance()
+	want := 10
+	assertCorrectBalance(t, got, want)
+}
