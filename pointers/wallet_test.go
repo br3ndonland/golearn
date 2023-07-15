4175f1ff1a40994d1828392e0ddedcc41fa323bb
Brendon Smith <bws@bws.bio>
2023-07-15 15:13:50 -0400

    Add derived type and method to pointers example

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -2,10 +2,10 @@ package pointers
 
 import "testing"
 
-func assertCorrectBalance(t testing.TB, got, want int) {
+func assertCorrectBalance(t testing.TB, got, want Bitcoin) {
 	t.Helper()
 	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %s want %s", got, want)
 	}
 }
 
@@ -13,6 +13,6 @@ func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 	wallet.Deposit(10)
 	got := wallet.Balance()
-	want := 10
+	want := Bitcoin(10)
 	assertCorrectBalance(t, got, want)
 }
